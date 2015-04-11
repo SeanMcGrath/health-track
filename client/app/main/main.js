74d67d29108ec0b1b5080f6f1bc7425ceb2a2875
SeanMcGrath <srmcgrat@umass.edu>
2015-04-11 16:12:49 -0400

    basic styling

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,6 +6,7 @@ angular.module('healthTrackApp')
       .state('main', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        authenticate: true
       });
   });
