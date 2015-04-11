031aa05c3c531f48d01ab65475e8d2e72430fabe
SeanMcGrath <srmcgrat@umass.edu>
2015-04-11 13:05:45 -0400

    editable name field

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,12 +12,21 @@ angular.module('healthTrackApp')
 
     $http.get('/api/trackers').success(function(trackers) {
       $scope.trackers = trackers;
+      trackers.forEach(function(tracker){
+        tracker.editable = false;
+      });
       socket.syncUpdates('tracker', $scope.trackers);
     });
 
-    $scope.deleteTracker = function(thing) {
-      $http.delete('/api/trackers/' + thing._id);
+    $scope.deleteTracker = function(tracker) {
+      $http.delete('/api/trackers/' + tracker._id);
     };
+
+    $scope.updateTracker = function(tracker) {
+      if(!tracker.editable){
+        $http.put('/api/trackers/' + tracker._id, tracker);
+      }
+    }
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('tracker');
